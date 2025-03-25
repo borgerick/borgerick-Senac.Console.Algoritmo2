679b60b695f34fe2f6c44b586c46dbc2a7525ec7
Erick Borges Rodrigues <borges.ericks@gmail.com>
2025-03-25 20:36:28 -0300

    CRIACAO DAS VARIAVEIS

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int idade = 10;
+string nomeCliente = "Erick Borges";
+decimal preco = 20M;
+char letra = 'a';
+var nomeCompleto = "Erick Borges Rodrigues";
+var altura = 167
