b6c6b6ef3e9169affa6ea68a7235820ca90f2161
Erick Borges Rodrigues <borges.ericks@gmail.com>
2025-03-25 21:50:20 -0300

    LEITURA DE VARIAVEIS

--- Algoritmo2/Algoritmo2/Program.cs
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -1,6 +1,21 @@
-﻿int idade = 10;
+﻿using System.Runtime.CompilerServices;
+
+int idade = 10;
 string nomeCliente = "Erick Borges";
 decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Erick Borges Rodrigues";
-var altura = 167
+var altura = 167;
+
+Console.WriteLine("Digite seu nome:");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo " + nomeCompleto);
+Console.Write("Digite sua idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("idade informada" + idade);
+if (idade < 18)
+    Console.WriteLine("Você não pode estar aqui");
+
+Console.ReadKey();
